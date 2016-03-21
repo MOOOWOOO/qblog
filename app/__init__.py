bf91034aac1b15cad1324b61013825e7ff204f50
mooowooo <mooowooo@live.com>
2016-03-21 08:58:21 +0800

    [Refactor](app): little modify.
    as title said

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,22 @@
 # coding: utf-8
-from config import config
 from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
+from .main.config import config
 
 __author__ = 'Jux.Liu'
 
 db = SQLAlchemy()
-login_manager=LoginManager()
+login_manager = LoginManager()
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app=app)
+
     db.init_app(app=app)
+    login_manager.init_app(app=app)
 
     from .main import main
     app.register_blueprint(main)
--- qblog.py
+++ qblog.py
@@ -1,9 +1,27 @@
 # coding: utf-8
-from app import create_app
+from app import create_app, db
+from app.user.models import User
+from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.script import Server, Shell, Manager
 
 __author__ = 'Jux.Liu'
 
 app = create_app('default')
 
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+manager.add_command('db', MigrateCommand)
+
+server = Server(host="0.0.0.0", port=5000, use_reloader=True)
+manager.add_command("runserver", server)
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    manager.run()
